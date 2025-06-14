bfc50eda579200d6aa237277002b9d6ee3802d74
Gabriel <gabriel.giro.resende@outlook.com>
2025-06-14 09:27:21 -0300

    feat: add dotenv

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,18 @@
+if (process.env.ENV === 'development') {
+  console.log('Running on development mode');
+  require('dotenv').config({ path: '../.env' });
+}
+
 import Fastify from 'fastify';
 import productRoutes from "./routes/products";
+import loginRoutes from './routes/login';
 import fastifyMultipart from '@fastify/multipart';
 
 const server = Fastify({logger: true});
 
 server.register(fastifyMultipart, { limits: { fileSize: 10 * 1024 * 1024 } });
 server.register(productRoutes, { prefix: '/products' });
+server.register(loginRoutes);
 
 server.listen({ port: 3000 }, (err, address) => {
   if (err) {
